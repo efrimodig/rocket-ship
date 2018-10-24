e1950486c86c6442c7da797bb4489b14af2e89b5
Erik Frimodig <frimodige@bsd405.org>
2018-10-23 19:55:22 -0700

    Update RocketShip.java

--- src/RocketShip.java
+++ src/RocketShip.java
@@ -17,14 +17,13 @@ public class RocketShip
 	
 	/* Part 2: Modify your main method to call the rocketLaunch method. The rocketLaunch method takes three integer
 	* parameters: minSize, maxSize and sizeStep that are defined as class constants. It will call the drawRocket
-	* method to print several rockets from maxSize to minSize decreasing the rocket size by sizeStep on each rocket
-	* so that the resulting output should look like the big rocket is chasing and eating the smaller rocket. The
-	* following example demonstrates a call to rocketLaunch(1, 5, 2) with 1 as minSize, 5 as maxSize and 2 as sizeStep.
-	* This will print the rockets sized 5, 3, and 1. The rocketLaunch method may assume the parameters are valid.
-	* In other words, it doesn't need to check for parameter error. It is important to note that all the rockets
-	* printed must be center justified. In order to do this, the drawrocket method must accept a second integer
-	* parameter leftPadding that specifies the number of spaces inserted to the left of the rocket so that it will
-	* be aligned with bigger rockets at the center column.
+	* method to print several rockets from minSize to maxSize, increasing the rocket size by sizeStep on each rocket
+	* so that the resulting output should look like the big rocket is chasing and the smaller rocket. See README.md
+	* for an example. 
+	* The rocketLaunch method may assume the parameters are valid. In other words, it doesn't need to check for 
+	* parameter error. It is important to note that all the rockets printed must be center justified. In order to
+	* do this, the drawrocket method must accept a second integer parameter leftPadding that specifies the number 
+	* of spaces inserted to the left of the rocket so that it will be aligned with bigger rockets at the center column.
 	*/
 	public static void drawRocket(int size, int leftPadding) {
 	}
